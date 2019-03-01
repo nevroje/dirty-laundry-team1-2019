5cb6ed98572123ad412ba846e1d8cf734df99fa1
Per Eriksson <per_hegg@hotmail.com>
2019-03-01 11:39:58 +0100

    add method to extract data from api and render it in the view

--- app/controllers/booking_controller.rb
+++ app/controllers/booking_controller.rb
@@ -1,7 +1,14 @@
 class BookingController < ApplicationController
   def index
     open_weather_api = Rails.configuration.open_weather_api
-    weather = open_weather_api.forecast :daily, city: "Stockholm", country_code: "se"
-    binding.pry
+    weather = open_weather_api.forecast :daily, city: "Stockholm", country_code: "SE"
+    @forecasts = []
+    
+    weather['list'].each do |forecast|
+      if forecast["dt_txt"].split(' ').last == "15:00:00"
+        @forecasts << forecast
+      end
+    end
+    # binding.pry
   end
 end
