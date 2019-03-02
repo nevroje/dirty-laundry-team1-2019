e50dd03312ee99cf49ecf718ff6165a8e865eeb4
nevroje <joakim.berglind@gmail.com>
2019-03-02 16:42:16 +0100

    added configs to env folder to test against mock add migrations changed syntax error in application html to match tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_01_131111) do
+ActiveRecord::Schema.define(version: 2019_03_02_153517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,20 @@ ActiveRecord::Schema.define(version: 2019_03_01_131111) do
     t.index ["booker_type", "booker_id"], name: "index_acts_as_bookable_bookings_booker"
   end
 
+  create_table "bookings", force: :cascade do |t|
+    t.string "name"
+    t.datetime "time_slot"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "forecasts", force: :cascade do |t|
+    t.float "lat"
+    t.float "lng"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "laundries", force: :cascade do |t|
     t.text "schedule"
     t.datetime "created_at", null: false
@@ -45,6 +59,8 @@ ActiveRecord::Schema.define(version: 2019_03_01_131111) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- features/support/omni_auth_fixtures.rb
+++ features/support/omni_auth_fixtures.rb
@@ -1,22 +1,22 @@
 module OmniAuthFixtures
   def self.facebook_mock
     {"provider"=>"facebook",
-          "uid"=>"2243663995747690",
+          "uid"=>"12434235235",
           "info"=>
-            {"email"=>"john.doe@mail.com",
-            "first_name"=>"John",
-            "last_name"=>"Doe",
-            "image"=>"http://graph.facebook.com/v2.10/"},
+            {"email"=>"grodon.ramsay@mail.com",
+            "first_name"=>"Grodon",
+            "last_name"=>"Ramsay",
+            "image"=>"jberg.png"},
           "credentials"=>
             {"token"=>
-                "EBADnlbiIiEkBACoOh2GeGbxbZABXGPPKQPZABVS9z3BjKIzlZB2hZBtTAYKZCwkO7HVchqY5ZCmrvvLk4i7GMwfuYVZAQsFK0JXreW6ngA7IgtsFYEeMKtBPZAVL1ZAEMWZCvVZCrZAmkcxwXIwnv1bEVZAZBVSCZBUsHdWsX74qAzZARm5t4QZDZD",
-            "expires_at"=>1554219256,
+                "123456789",
+            "expires_at"=>12345,
             "expires"=>true},
           "extra"=>
             {"raw_info"=>
-                {"first_name"=>"John",
-              "last_name"=>"Doe",
-              "email"=>"john.doe@mail.com",
-              "id"=>"2243663995747690"}}}
+                {"first_name"=>"Grodon",
+              "last_name"=>"Ramsay",
+              "email"=>"grodon.ramsay@mail.com",
+              "id"=>"21235235"}}}
   end
 end
