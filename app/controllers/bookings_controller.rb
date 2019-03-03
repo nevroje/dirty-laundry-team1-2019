02da2f26007cf7e2d141739c60f272c964405829
Laura Reale <laaru1988@gmail.com>
2019-03-03 12:19:15 +0100

    add user can become a subscriber feature, add user role limitation in bookings controller create method, refactor basic steps and assertions

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,11 +5,15 @@ class BookingsController < ApplicationController
   end
 
   def create
-    start_date_time = DateTime.parse(params[:slot])
-    if current_user.book! @laundry, time: start_date_time, amount: 1
-      redirect_to root_path, notice: "Booking was successfully created."
+    if current_user.bookings.length < 1 || (current_user.is_subscriber? && current_user.bookings.length < 4)
+      start_date_time = DateTime.parse(params[:slot])
+      if current_user.book! @laundry, time: start_date_time, amount: 1
+        redirect_to root_path, notice: "Booking was successfully created."
+      else
+        redirect_to root_path, error: "This time-slot has been booked!"
+      end
     else
-      redirect_to root_path, error: "This time-slot has been booked!"
-    end
+      redirect_to root_path, error: "You reached your booking limit!"
+    end 
   end
 end
--- features/step_definitions/assertions.rb
+++ features/step_definitions/assertions.rb
@@ -5,3 +5,7 @@ end
 Then("I should be redirected to index page") do
   expect(current_path).to eq root_path 
 end
+
+Then("(s/S)how me the page") do
+  save_and_open_page
+end
