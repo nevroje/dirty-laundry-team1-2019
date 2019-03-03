7291549589824872077ce15662d568a39cf07c75
nevroje <joakim.berglind@gmail.com>
2019-03-03 21:19:34 +0100

    added credentials and keys after accepting merges

--- config/initializers/open-weather-api.rb
+++ config/initializers/open-weather-api.rb
@@ -1,6 +1,6 @@
 OpenWeatherAPI.configure do |config|
     # API key
-    config.api_key = Rails.application.credentials.weather_api[:api_key]
+    #config.api_key = Rails.application.credentials.weather_api[:api_key]
   
     # Optionals
     config.default_language = 'en'     # 'en' by default
