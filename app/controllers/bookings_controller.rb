eca5cd63470dc04bd1017ffe885b7a9d9c76c973
nevroje <joakim.berglind@gmail.com>
2019-03-03 21:35:21 +0100

    refactored controllers and setup api key env

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,8 +1,19 @@
 class BookingsController < ApplicationController
   before_action :authenticate_user!, only: :create
   def index
+
     @laundry = Laundry.first
+    open_weather_api = Rails.configuration.open_weather_api
+    weather = open_weather_api.forecast :hourly, city: "Stockholm", country_code: "se", default_unit: "metric"
+    @forecasts = []
+    
+    weather['list'].each do |forecast|
+      if forecast["dt_txt"].split(' ').last == "15:00:00"
+        @forecasts << forecast
+      end
+    end
   end
+
 
   def create
     if current_user.bookings.length < 1 || (current_user.subscriber? && current_user.bookings.length < 4)
